ed9a03e4f12c143ba4821030c5284f4766d612eb
Jerraloid <jerryblaauw@hotmail.com>
2017-11-17 18:37:37 +0100

    l

--- fiets/src/main/java/org/jerraloid/fiets/chat/server/ChatServerHandler.java
+++ fiets/src/main/java/org/jerraloid/fiets/chat/server/ChatServerHandler.java
@@ -1,12 +1,24 @@
 package org.jerraloid.fiets.chat.server;
 
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 public class ChatServerHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) {
-		
+		try {
+			Charset utf8 = CharsetUtil.UTF_8;
+			String in = ((ByteBuf)msg).toString(utf8);
+			
+			System.out.println(in);
+		}
+		finally {
+			
+		}
 	}
 }
